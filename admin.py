71c2e3b7de2c2c4941db7a04293e85dc7707dc24
Andrew <wabarr@users.noreply.github.com>
2024-12-16 16:04:56 -0500

    add admin action for CSV output

--- admin.py
+++ admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from whoishere.models import Course, Checkin, AttendancePoll
+import csv
+from django.http import HttpResponse
 
 class CourseAdmin(admin.ModelAdmin):
     fields = ["nickname", "instructor", "semester", "year", "subject_code", "course_number"]
@@ -11,6 +13,23 @@ class CheckinAdmin(admin.ModelAdmin):
     list_display = fields
     list_filter = ["attendance_poll"]
     search_fields = ["student_last_name", "student_first_name", "gw_email_handle", "GWID"]
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 class AttendanceCheckAdmin(admin.ModelAdmin):
     fields = ["slug", "course", "starts", "expires"]
